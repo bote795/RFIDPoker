b5498132dde339464b9ad2af24691ac247d15fd0
bote795 <nbotell2@gmail.com>
2015-10-28 18:14:19 -0500

    first commit

---
+++ RFIDReader.ino
@@ -0,0 +1,223 @@
+
+/*****************************
+     RFID-powered lockbox
+
+This sketch will move a servo when
+a trusted tag is read with the 
+ID-12/ID-20 RFID module
+
+Pinout for SparkFun RFID USB Reader
+Arduino ----- RFID module
+5V            VCC
+GND           GND
+D2            TX
+
+Pinout for SparkFun RFID Breakout Board
+Arduino ----- RFID module
+5V            VCC
+GND           GND
+D2            D0
+
+Connect the servo's power, ground, and
+signal pins to VCC, GND,
+and Arduino D9
+
+If using the breakout, you can also 
+put an LED & 330 ohm resistor between 
+the RFID module's READ pin and GND for 
+a "card successfully read" indication
+
+by acavis, 3/31/2015
+
+
+ LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ 
+Inspired by & partially adapted from
+http://bildr.org/2011/02/rfid-arduino/
+
+******************************/
+
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#include <Key.h>
+#include <Keypad.h>
+
+
+
+// Choose two pins for SoftwareSerial
+SoftwareSerial rSerial(10,11); // RX, TX
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12,13, 5, 4, 3, 2);
+
+// For SparkFun's tags, we will receive 16 bytes on every
+// tag read, but throw four away. The 13th space will always
+// be 0, since proper strings in Arduino end with 0
+
+// These constants hold the total tag length (tagLen) and
+// the length of the part we want to keep (idLen),
+// plus the total number of tags we want to check against (kTags)
+const int tagLen = 16;
+const int idLen = 13;
+const int kTags = 6;
+
+// Put your known tags here!
+char knownTags[kTags][idLen] = {
+             "111111111111",
+             "444444444444",
+             "555555555555",
+             "7A005B0FF8D6",
+             "7C00572B4242",
+             "7C0057306A71"
+};
+
+// Empty array to hold a freshly scanned tag
+char newTag[idLen];
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 3; // Three columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 40, 42,44, 46 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 34, 36, 38 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+void setup() {
+  // Starts the hardware and software serial ports
+   Serial.begin(9600);
+   rSerial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(20, 4);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+  Serial.println("Booting up!");
+
+
+}
+
+void loop() {
+  // Counter for the newTag array
+  int i = 0;
+  // Variable to hold each byte read from the serial buffer
+  int readByte;
+  // Flag so we know when a tag is over
+  boolean tag = false;
+
+char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+    switch (key)
+    {
+      case '*':
+        Serial.println("*");
+        break;
+      case '#':
+        Serial.println("#");
+        break;
+      default:
+        Serial.println(key);
+    }
+  }
+
+
+
+  // This makes sure the whole tag is in the serial buffer before
+  // reading, the Arduino can read faster than the ID module can deliver!
+ // Serial.println(rSerial.available());
+  if (rSerial.available() == tagLen) {
+    tag = true;
+    Serial.println("avilable tag");
+  }
+
+  if (tag == true) {
+    while (rSerial.available()) {
+      // Take each byte out of the serial buffer, one at a time
+      readByte = rSerial.read();
+      Serial.println("reading tag");
+      /* This will skip the first byte (2, STX, start of text) and the last three,
+      ASCII 13, CR/carriage return, ASCII 10, LF/linefeed, and ASCII 3, ETX/end of 
+      text, leaving only the unique part of the tag string. It puts the byte into
+      the first space in the array, then steps ahead one spot */
+      if (readByte != 2 && readByte!= 13 && readByte != 10 && readByte != 3) {
+        newTag[i] = readByte;
+        i++;
+      }
+
+      // If we see ASCII 3, ETX, the tag is over
+      if (readByte == 3) {
+        tag = false;
+      }
+
+    }
+  }
+
+
+  // don't do anything if the newTag array is full of zeroes
+  if (strlen(newTag)== 0) {
+    return;
+  }
+
+  else {
+    int total = 0;
+    Serial.println("looking for tag");
+    for (int ct=0; ct < kTags; ct++){
+        total += checkTag(newTag, knownTags[ct]);
+    }
+
+    // If newTag matched any of the tags
+    // we checked against, total will be 1
+    if (total > 0) {
+
+      // Put the action of your choice here!
+      
+      // set the cursor to column 0, line 1
+      // (note: line 1 is the second row, since counting begins with 0):
+      lcd.setCursor(0, 1);
+      lcd.print("Success!");
+      lcd.setCursor(0, 2);
+      lcd.print(newTag);
+       Serial.println("Success!");
+    }
+
+    else {
+        // This prints out unknown cards so you can add them to your knownTags as needed
+        Serial.print("Unknown tag! ");
+        Serial.print(newTag);
+        Serial.println();
+    }
+  }
+
+  // Once newTag has been checked, fill it with zeroes
+  // to get ready for the next tag read
+  for (int c=0; c < idLen; c++) {
+    newTag[c] = 0;
+  }
+}
+
+// This function steps through both newTag and one of the known
+// tags. If there is a mismatch anywhere in the tag, it will return 0,
+// but if every character in the tag is the same, it returns 1
+int checkTag(char nTag[], char oTag[]) {
+    for (int i = 0; i < idLen; i++) {
+      if (nTag[i] != oTag[i]) {
+        return 0;
+      }
+    }
+  return 1;
+}
+
