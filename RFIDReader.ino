e0921edc43f609ed4957a2d216f8c6630ef3d827
bote795 <nbotell2@gmail.com>
2015-11-18 22:00:09 -0600

    working on printing to screen and wrapping

--- RFIDReader.ino
+++ RFIDReader.ino
@@ -47,11 +47,12 @@ http://bildr.org/2011/02/rfid-arduino/
 #include <LiquidCrystal.h>
 #include <Key.h>
 #include <Keypad.h>
+#include <math.h>
 
 
 
 // Choose two pins for SoftwareSerial
-SoftwareSerial rSerial(10,11); // RX, TX
+SoftwareSerial rSerial(10,11); // RX, TX    input ten 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12,13, 5, 4, 3, 2);
 
@@ -64,16 +65,117 @@ LiquidCrystal lcd(12,13, 5, 4, 3, 2);
 // plus the total number of tags we want to check against (kTags)
 const int tagLen = 16;
 const int idLen = 13;
-const int kTags = 6;
-
+const int kTags = 52;
+//hearts, diamons, spades, clubs
 // Put your known tags here!
 char knownTags[kTags][idLen] = {
-             "111111111111",
-             "444444444444",
-             "555555555555",
-             "7A005B0FF8D6",
-             "7C00572B4242",
-             "7C0057306A71"
+             "09001738DCFA", //1s //A spade
+             "090016FCA645", //2s
+             "090017268FB7", //3s
+             "09001677761E", //4s
+             "0900331EE2C6", //5s
+             "090033FDF334", //6s
+             "090016AE8E3F", //7s
+             "09003449A3D7",//8s
+             "0900330A5161",//9s
+             "090032ED4096",//10s
+             "090033FF2BEE",//11s J
+             "09001731E4CB",//12s Q
+             "0900345B1573",//13s K
+             "090034AA23B4", // 1c Ace
+             "090033861BA7", //2c //2 clubs
+             "09001715AAA1", // 3c 
+             "0900347A7B3C", // 4c 
+             "0900169873F4", // 5c
+             "09003449D0A4", // 6c
+             "0900343EA1A2", // 7c
+             "090018418FDF", // 8c
+             "090016AC9C2F", //9c  // 9 clubs
+             "0900342DABBB", // 10c
+             "090033DFF316", // 11c J
+             "0900338648F4", // 12c Q
+             "09003386922E", // 13c K
+             "0900344F3C4E", //Ah //A hearts
+             "090017268BB3", //2h  //2 hearts   
+             "09003309281B", //3h
+             "090034366962", //4h
+             "0900334FA3D6", //5h
+             "09003449C0B4", //6h
+             "090034587015", //7h
+             "090034365C57", //8h
+             "090033204953", //9h
+             "090017083C2A", //10h
+             "0900349F9537", //11h
+             "090034790B4F", //12h
+             "0900175E1757", //13h
+             "090016C1CB15", //1d    
+             "090016D34B87", //2d   
+             "090017E315E8", //3d   
+             "09003480EF52", //4d 
+             "0900172F2312", //5d 
+             "090033EF09DC", //6d
+             "09003406556E", //7d 
+             "0900336698C4", //8d 
+             "0900334A4434", //9d 
+             "09003446D1AA", //10d 
+             "090033E64E92", //11d 
+             "0900343BD8DE", //12d 
+             "090033DE7C98" //13d    
+};
+
+char keyTags[kTags][4] = {
+  "s1", 
+  "s2",
+  "s3",
+  "s4",
+  "s5",
+  "s6",
+  "s7",
+  "s8",
+  "s9",
+  "s10",
+  "s11",
+  "s12",
+  "s13",
+  "c1",
+  "c2",
+  "c3",
+  "c4",
+  "c5",
+  "c6",
+  "c7",
+  "c8",
+  "c9", 
+  "c10",
+  "c11",
+  "c12",
+  "c13",
+  "h1",
+  "h2", 
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "h7",
+  "h8",
+  "h9",
+  "h10",
+  "h11",
+  "h12",
+  "h13",
+  "d1",
+  "d2",  
+  "d3",  
+  "d4",
+  "d5", 
+  "d6",
+  "d7",
+  "d8",
+  "d9",
+  "d10", 
+  "d11", 
+  "d12", 
+  "d13"   
 };
 
 // Empty array to hold a freshly scanned tag
@@ -89,9 +191,9 @@ char keys[ROWS][COLS] = {
   {'*','0','#'}
 };
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 46, 48,51, 52 };
+byte rowPins[ROWS] = { 39, 41,43, 45 };
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 41, 43, 45 };
+byte colPins[COLS] = { 33, 35, 37 };
 
 // Create the Keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
@@ -117,16 +219,16 @@ void loop() {
   int readByte;
   // Flag so we know when a tag is over
   boolean tag = false;
-
-  int inputInt = readFromKeypad();
+/*
+  long inputInt = readFromKeypad();
   if(inputInt != 0)
   {
     Serial.print("this is the main loop bro ");
     Serial.println(inputInt);  
   }  
 
-
-
+*/
+  writeAndRead("How much do you call");
 
   // This makes sure the whole tag is in the serial buffer before
   // reading, the Arduino can read faster than the ID module can deliver!
@@ -217,16 +319,40 @@ int checkTag(char nTag[], char oTag[]) {
 //waits for input and returns it
 long writeAndRead(String text)
 {
-    lcd.setCursor(0, 1);
-    lcd.print(text);
-    lcd.setCursor(0, 2);
-   // return readFromKeypad();   
+    //when text is greater than 20 wrap
+    int lines =round(text.length()/20);
+    String textArray[3];
+    int cursorValue =splitString(text, textArray ,lines);
+    
+    long inputInt = readFromKeypad(lines, cursorValue );
+    if(inputInt != -1)
+    {
+      Serial.print("this is the main loop bro ");
+      Serial.println(inputInt);  
+    }  
+    return inputInt;   
 }
 
+//text = string to be printed
+//ArrayText = array of empty strings with returned strings
+int splitString(String text, String *arrayText, int lines)
+{
+  int end=0;
+  for(int i=0; i < lines && lines < 5; i++)
+  {
+    end = ((i+1) * 20)+i;
+    if( text.length() < end)
+        end= text.length();
+    arrayText[i]= text.substring((i*20)+i,end);   
+    lcd.setCursor(0, i+1);
+    lcd.print(arrayText[i]);
+  }
+  return end;
+}
 //function used to read from keyPad
 //# = used for enter
 //* =  used for backspace
-long readFromKeypad()
+long readFromKeypad(int lines, int cursorValue)
 {
   char input[10];
   long i=0;
@@ -254,6 +380,9 @@ long readFromKeypad()
             
           }
         }
+        lcd.setCursor(0, lines+1);
+        lcd.print("                     ");
+        lcd.print(atol(input));
         Serial.println(atol(input));
         //lcd.print(atol(input));
         
@@ -261,6 +390,10 @@ long readFromKeypad()
        key = keypad.getKey();
      }
   }
+  else
+    return -1;
+
+  
   return atol(input);
 }
 
